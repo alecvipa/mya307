978ba2f13691985e0b73838f4b98a8a9c2ea8c3a
alecvipa <hebrit_626@hotmail.com>
2022-08-10 09:54:24 -0500

    added payment methods

--- server.js
+++ server.js
@@ -77,7 +77,10 @@ app.post("/ajax/email", function (request, response) {
 
 
     var htmlBody = `<h2>Correo de contacto</h2><p>Name: ${request.body.name} </p> <p>Phone: ${request.body.number} </p> <p> e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Arrive date: ${request.body.arriveDate} </p><p>Departure: ${request.body.departDate} </p> <p>Number of extra people: ${request.body.people} </p>`;
-    var secondHtmlBody = `<h2>Here are the details of your booking!</h2><p>Reservation by the name: ${request.body.name} </p> <p>Your phone number: ${request.body.number} </p> <p> Your e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Desired arrival date: ${request.body.arriveDate} </p><p>Desired Departure date: ${request.body.departDate} </p> <p>People coming with you: ${request.body.people} </p> <h5>Our team will contact you shortly to set the last details of your stay. </h5>`
+    var secondHtmlBody = `<h2>Here are the details of your booking!</h2><p>Reservation by the name: ${request.body.name} </p> <p>Your phone number: ${request.body.number} </p> <p> Your e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Desired arrival date: ${request.body.arriveDate} </p><p>Desired Departure date: ${request.body.departDate} </p> <p>People coming with you: ${request.body.people} </p><hr> <h5>Our team will contact you shortly to set the last details of your stay. </h5> <h5>Payment methods:</h5><h5>Debit / credit & bank
+    transfer.</h5><h5>We usually ask for a percentage prior your arrival and you can pay the rest of your stay
+    upon your arrival.</h5><h5>We accept cash at the front desk or you may pay in the above mentioned payment
+    methods.</h5>`
     let customerMail = `${request.body.email}`
     var mail = {
         from: 'Team: Xyncs Web Studio',
@@ -124,7 +127,10 @@ app.post("/ajax/emailSecond", function (request, response) {
 
 
     var htmlBody = `<h2>Correo de contacto</h2><p>Name: ${request.body.name} </p> <p>Phone: ${request.body.number} </p> <p> e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Arrive date: ${request.body.arriveDate} </p><p>Departure: ${request.body.departDate} </p> <p>Number of extra people: ${request.body.people} </p>`;
-    var secondHtmlBody = `<h2>Here are the details of your booking!</h2><p>Reservation by the name: ${request.body.name} </p> <p>Your phone number: ${request.body.number} </p> <p> Your e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Desired arrival date: ${request.body.arriveDate} </p><p>Desired Departure date: ${request.body.departDate} </p> <p>People coming with you: ${request.body.people} </p> <h5>Our team will contact you shortly to set the last details of your stay. </h5>`
+    var secondHtmlBody = `<h2>Here are the details of your booking!</h2><p>Reservation by the name: ${request.body.name} </p> <p>Your phone number: ${request.body.number} </p> <p> Your e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Desired arrival date: ${request.body.arriveDate} </p><p>Desired Departure date: ${request.body.departDate} </p> <p>People coming with you: ${request.body.people} </p><hr> <h5>Our team will contact you shortly to set the last details of your stay. </h5> <h5>Payment methods:</h5><h5>Debit / credit & bank
+    transfer.</h5><h5>We usually ask for a percentage prior your arrival and you can pay the rest of your stay
+    upon your arrival.</h5><h5>We accept cash at the front desk or you may pay in the above mentioned payment
+    methods.</h5>`
     let customerMail = `${request.body.email}`
     var mail = {
         from: 'Team: Xyncs Web Studio',
